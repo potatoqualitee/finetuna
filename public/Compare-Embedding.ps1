5194081cb7eb7531c31216162410a210c173f2eb
Chrissy LeMaire <clemaire@gmail.com>
2024-06-21 07:16:58 +0200

    remove mathnet.numerics requirement

--- public/Compare-Embedding.ps1
+++ public/Compare-Embedding.ps1
@@ -1,15 +1,14 @@
 function Compare-Embedding {
-<#
+    <#
     .SYNOPSIS
-    Calculates the cosine similarity between two embedding vectors.
+    Calculates the similarity between two embedding vectors.
 
     .DESCRIPTION
     The Compare-Embedding command takes a query embedding vector and searches for the most similar text embeddings within a collection.
-
-    It returns the top N most similar results based on cosine similarity.
+    It returns the top N most similar results based on the chosen similarity measure.
 
     .PARAMETER Query
-    Some text
+    Some text to be converted to an embedding.
 
     .PARAMETER QueryEmbedding
     The query embedding vector to search with.
@@ -18,18 +17,25 @@ function Compare-Embedding {
     The collection of text embeddings to search through.
 
     .PARAMETER Top
-    Optional. The number of top similar results to return. Default is 5.
+    Optional. The number of top similar results to return. Default is 25.
+
+    .PARAMETER Type
+    The type of similarity measure to use. Options are 'Cosine', 'Euclidean', 'Pearson', 'SquaredEuclidean'. Default is 'Cosine'.
 
     .EXAMPLE
     $queryEmbedding = Get-Embedding -Query "brown fox"
-    $embeddings = "The quick brown fox", "A lazy dog", "A cunning fox" | Get-Embedding
+    $embeddings = @{
+        "The quick brown fox" = (Get-Embedding "The quick brown fox");
+        "A lazy dog" = (Get-Embedding "A lazy dog");
+        "A cunning fox" = (Get-Embedding "A cunning fox")
+    }
     Compare-Embedding -QueryEmbedding $queryEmbedding -Embeddings $embeddings
 
     This command searches for the most similar text embeddings to the query "brown fox".
 
     .EXAMPLE
     $queryEmbedding = Get-Embedding -Query "playful dog"
-    $embeddings = Get-Content "descriptions.txt" | Get-Embedding
+    $embeddings = Get-Content "descriptions.txt" | ForEach-Object { @{$_ = Get-Embedding $_} }
     Compare-Embedding -QueryEmbedding $queryEmbedding -Embeddings $embeddings -Top 3
 
     This command searches through a collection of embeddings created from a text file and returns the top 3 most similar results to the query.
@@ -53,53 +59,67 @@ function Compare-Embedding {
             $QueryEmbedding = Get-Embedding -Text $Query
         }
 
-        $queryVector = [MathNet.Numerics.LinearAlgebra.Double.DenseVector]::OfArray($QueryEmbedding)
         $similarities = New-Object System.Collections.ArrayList
 
         foreach ($key in $Embeddings.Keys) {
-            try {
-                $embeddingVector = [MathNet.Numerics.LinearAlgebra.Double.DenseVector]::OfArray($Embeddings[$key])
-            } catch {
-                Write-Verbose "Error with $key"
-                continue
-            }
+            $embeddingVector = $Embeddings[$key]
             $similarity = 0
 
             switch ($Type) {
                 'Cosine' {
-                    # Correctly calculate cosine similarity using dot product and norms
-                    $dotProduct = $queryVector.DotProduct($embeddingVector)
-                    $normsProduct = $queryVector.L2Norm() * $embeddingVector.L2Norm()
-                    if ($normsProduct -ne 0) {
-                        $similarity = $dotProduct / $normsProduct
-                    } else {
-                        $similarity = 0
+                    $dotProduct = 0
+                    $normQuery = 0
+                    $normEmbedding = 0
+                    for ($i = 0; $i -lt $QueryEmbedding.Length; $i++) {
+                        $dotProduct += $QueryEmbedding[$i] * $embeddingVector[$i]
+                        $normQuery += $QueryEmbedding[$i] * $QueryEmbedding[$i]
+                        $normEmbedding += $embeddingVector[$i] * $embeddingVector[$i]
+                    }
+                    $normQuery = [Math]::Sqrt($normQuery)
+                    $normEmbedding = [Math]::Sqrt($normEmbedding)
+                    if ($normQuery -ne 0 -and $normEmbedding -ne 0) {
+                        $similarity = $dotProduct / ($normQuery * $normEmbedding)
                     }
                 }
                 'Euclidean' {
-                    # Correct Euclidean distance calculation
-                    $distanceVector = $queryVector.Subtract($embeddingVector)
-                    $distance = $distanceVector.L2Norm()
+                    $distance = 0
+                    for ($i = 0; $i -lt $QueryEmbedding.Length; $i++) {
+                        $diff = $QueryEmbedding[$i] - $embeddingVector[$i]
+                        $distance += $diff * $diff
+                    }
+                    $distance = [Math]::Sqrt($distance)
                     $similarity = 1 / (1 + $distance)
                 }
                 'Pearson' {
-                    $keyEmbedding = [double[]]($Embeddings[$key])
-                    # Pearson correlation calculation appears correct
-                    $correlation = [MathNet.Numerics.Statistics.Correlation]::Pearson($QueryEmbedding, $keyEmbedding)
-                    $similarity = ($correlation + 1) / 2
+                    $meanQuery = ($QueryEmbedding | Measure-Object -Average).Average
+                    $meanEmbedding = ($embeddingVector | Measure-Object -Average).Average
+                    $covariance = 0
+                    $varQuery = 0
+                    $varEmbedding = 0
+                    for ($i = 0; $i -lt $QueryEmbedding.Length; $i++) {
+                        $diffQuery = $QueryEmbedding[$i] - $meanQuery
+                        $diffEmbedding = $embeddingVector[$i] - $meanEmbedding
+                        $covariance += $diffQuery * $diffEmbedding
+                        $varQuery += $diffQuery * $diffQuery
+                        $varEmbedding += $diffEmbedding * $diffEmbedding
+                    }
+                    if ($varQuery -ne 0 -and $varEmbedding -ne 0) {
+                        $correlation = $covariance / ([Math]::Sqrt($varQuery) * [Math]::Sqrt($varEmbedding))
+                        $similarity = ($correlation + 1) / 2
+                    }
                 }
                 'SquaredEuclidean' {
-                    # Adjust Squared Euclidean distance calculation
-                    $distanceVector = $queryVector.Subtract($embeddingVector)
-                    $distance = $distanceVector.L2Norm()
-                    $distanceSquared = $distance * $distance
+                    $distanceSquared = 0
+                    for ($i = 0; $i -lt $QueryEmbedding.Length; $i++) {
+                        $diff = $QueryEmbedding[$i] - $embeddingVector[$i]
+                        $distanceSquared += $diff * $diff
+                    }
                     $similarity = 1 / (1 + $distanceSquared)
                 }
             }
             $null = $similarities.Add([PSCustomObject]@{ Command = $key; Similarity = $similarity })
         }
 
-
-        $similarities |  Sort-Object -Property Similarity -Descending | Select-Object -First $Top
+        $similarities | Sort-Object -Property Similarity -Descending | Select-Object -First $Top
     }
 }
